7ae1d8b6e339c971e96c25ce006ea08d888b79ad
Chase <chase.anderson.000@gmail.com>
2022-03-24 02:16:54 -0500

    colorSense
    2 methods to implement

---
+++ colorSensor/testOn/testOn.ino
@@ -0,0 +1,67 @@
+#include <Wire.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Wire.begin();
+  Serial.begin(9600);
+
+}
+
+byte addr = 0x29;
+
+byte readL;
+byte readH;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Wire.beginTransmission(addr);
+  Wire.write(byte(0x00));
+  Wire.write(byte(0x0B));
+  Wire.endTransmission();
+
+  // Wire.beginTransmission(addr);
+
+  // Wire.write(byte(0x01));
+  // Wire.write(byte(0xF6));
+
+  // Wire.endTransmission();
+
+  delay(10);
+
+  Wire.beginTransmission(addr);
+
+  Wire.write(byte(0x17));
+
+  Wire.requestFrom(addr, 1);
+
+  if(1 <= Wire.available()){
+
+    readL = Wire.read();
+
+  }
+
+  Wire.endTransmission();
+
+  // Wire.beginTransmission(addr);
+
+  // Wire.write(byte(0x17));
+
+  // Wire.endTransmission();
+
+  // Wire.requestFrom(addr, 1);
+
+  // if(Wire.available() >= 1){
+
+  //   readH = Wire.read();
+
+  // }
+
+  Serial.print("0x");
+  Serial.println(readL, HEX);
+  // Serial.println(readL);
+  
+  delay(1000);
+
+}
